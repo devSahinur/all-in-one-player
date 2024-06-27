e97c41030add30d1df46ddf8f6e84e6eb994f934
Sahinur Islam <infosahinur@gmail.com>
2024-06-27 17:37:14 +0600

    Update videoPlayer.js

--- components/videoPlayer.js
+++ components/videoPlayer.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 
-const VideoPlayer = ({ videoUrl }) => {
+const VideoPlayer = ({ videoUrl="https://catchup.yuppcdn.net/amazonv2/36/preview/colorsbanglahd/master/chunklist.m3u8" }) => {
   return (
     <div className="flex justify-center items-center w-full py-8">
       <div className="relative w-full max-w-2xl aspect-w-16 aspect-h-9 bg-black rounded-lg overflow-hidden shadow-lg">
